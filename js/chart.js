6a37dc4dbd5c86e402536890d50dff3ded99f378
Aaron Clark <amclarkwustl@gmail.com>
2023-05-18 14:27:19 -0700

    added chart functionality

--- js/chart.js
+++ js/chart.js
@@ -2,6 +2,8 @@
 
 let canvasElem = document.getElementById('chart')
 
+
+
 /* TODO:
  * - Instantiate a new AppState
  * - Use a method on that AppState to load vote data from localStorage.
@@ -10,7 +12,70 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
+let state = new AppState();
+state.loadItems();
+
 function renderChart() {
+
+  let productNames = [];
+  let productVotes = [];
+  let productViews = [];
+
+  for (let i=0; i < state.allProducts.length; i++){
+    productNames.push(state.allProducts[i].name);
+    productVotes.push(state.allProducts[i].timesClicked);
+    productViews.push(state.allProducts[i].timesShown);
+  }
+
+  let chartObj = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Views',
+        data: productViews,
+        borderWidth: 3,
+        borderColor: '#8b008b',
+        backgroundColor: 'rgb(204, 153, 255)'
+      },
+      {
+        label: '# of Votes',
+        data: productVotes,
+        borderWidth: 3,
+        borderColor: 'rgb(204, 85, 0)',
+        backgroundColor: 'rgb(255, 204, 153)'
+      }
+      ]
+    },
+    options: {
+      plugins: {
+        tooltip: {
+          enabled: true,
+          mode: 'nearest',
+          backgroundColor: 'rgb(0, 0, 139)',
+
+
+        },
+        title: {
+          display: true,
+          text: 'Odd Duck Product Voting Results'
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          display: true,
+          text: 'Votes'
+        },
+        x: {
+          text: 'Product Name',
+          display: true
+        }
+      }
+    }
+  };
+  new Chart(canvasElem, chartObj);
 }
 
 renderChart();
